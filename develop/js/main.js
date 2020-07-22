b700a196440e8e836024d04dc54dda1045be2f3b
Joe Brimeyer <jbrimeyer@gmail.com>
2020-07-22 16:21:43 -0700

    Text color function and about page.

--- develop/js/main.js
+++ develop/js/main.js
@@ -3,7 +3,7 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll("nav-item");
+const navItems = document.querySelectorAll(".nav-item");
 
 // Set Initial State of the Menu
 
